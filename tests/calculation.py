7233fd00d301d65b0dc768150bbbb6edff7ad292
DennyL <denis.lozinskij@gmail.com>
2020-03-14 19:42:54 +0200

    Calculation tests are run now from a separate file in the tests folder. The core code was refactored and cleaned up correspondingly

--- tests/calculation.py
+++ tests/calculation.py
@@ -1,3 +1,10 @@
+##################
+#  TESTING AREA  #
+##################
+
+from src.iShare4 import main
+
+
 def test_1():
     data = ([["Den", 20], ["Fox", 40], ["Zoe", 0], ["Andy", 0], ["Bron", 40]],
             100)
@@ -10,7 +17,8 @@ def test_2():
     data = ([["Den", 5], ["Fox", 0], ["Zoe", 15], ["Andy", 30], ["Bron", 50]],
             100)
     output = main(*data)
-    assert output == "\n>>>Equal share is 20.00<<<\nFox pays 20.00 to Bron\nDen pays 10.00 to Andy\nDen pays 5.00 to Bron\nZoe pays 5.00 to Bron"
+    assert output == "\n>>>Equal share is 20.00<<<\nFox pays 20.00 to Bron\nDen pays 10.00 to Andy\nDen pays 5.00 to " \
+                     "Bron\nZoe pays 5.00 to Bron"
     return output
 
 
@@ -18,7 +26,8 @@ def test_3():
     data = ([["Den", 0], ["Fox", 85], ["Zoe", 0], ["Andy", 15], ["Bron", 0]],
             100)
     output = main(*data)
-    assert output == "\n>>>Equal share is 20.00<<<\nDen pays 20.00 to Fox\nZoe pays 20.00 to Fox\nBron pays 20.00 to Fox\nAndy pays 5.00 to Fox"
+    assert output == "\n>>>Equal share is 20.00<<<\nDen pays 20.00 to Fox\nZoe pays 20.00 to Fox\nBron pays 20.00 to " \
+                     "Fox\nAndy pays 5.00 to Fox"
     return output
 
 
@@ -26,7 +35,8 @@ def test_4():
     data = ([["Harry", 0], ["Den", 180], ["Fox", 400], ["Chad", 23],
              ["Elena", 900], ["July", 77]], 1580)
     output = main(*data)
-    assert output == "\n>>>Equal share is 263.33<<<\nHarry pays 263.33 to Elena\nChad pays 136.67 to Fox\nChad pays 103.67 to Elena\nJuly pays 186.33 to Elena\nDen pays 83.33 to Elena"
+    assert output == "\n>>>Equal share is 263.33<<<\nHarry pays 263.33 to Elena\nChad pays 136.67 to Fox\nChad pays " \
+                     "103.67 to Elena\nJuly pays 186.33 to Elena\nDen pays 83.33 to Elena"
     return output
 
 
@@ -41,7 +51,8 @@ def test_6():
     data = ([["Ed", 1083.12], ["Den", 70], ["Artem", 600], ["Dasha", 0]],
             1753.12)
     output = main(*data)
-    assert output == "\n>>>Equal share is 438.28<<<\nDasha pays 438.28 to Ed\nDen pays 161.72 to Artem\nDen pays 206.56 to Ed"
+    assert output == "\n>>>Equal share is 438.28<<<\nDasha pays 438.28 to Ed\nDen pays 161.72 to Artem\nDen pays " \
+                     "206.56 to Ed"
     return output
 
 
@@ -57,16 +68,16 @@ def test_8():
     data = ([["Елена", 187], ["Таня", 1500], ["Алекс", 400], ["Толя", 0],
              ["Оля", 1000], ["Денис", 950], ["Вася", 1950]], 5987)
     output = main(*data)
-    assert output == "\n>>>Equal share is 855.29<<<\nТоля pays 855.29 to Вася\nЕлена pays 644.71 to Таня\nЕлена pays 23.57 to Оля\nАлекс pays 94.71 to Денис\nАлекс pays 239.43 to Вася\nАлекс pays 121.14 to Оля"
+    assert output == "\n>>>Equal share is 855.29<<<\nТоля pays 855.29 to Вася\nЕлена pays 644.71 to Таня\nЕлена pays " \
+                     "23.57 to Оля\nАлекс pays 94.71 to Денис\nАлекс pays 239.43 to Вася\nАлекс pays 121.14 to Оля"
     return output
 
-#iShare_4_beta.controller()
 
-##    print(test_1())
-##    print(test_2())
-##    print(test_3())
-##    print(test_4())
-##    print(test_5())
-##    print(test_6())
-##    print(test_7())
-##    print(test_8())
+def runner():
+    for test_number in range(8):
+        test_name = "test_{}()".format(test_number + 1)
+        print(eval(test_name))
+
+
+if __name__ == "__main__":
+    runner()
